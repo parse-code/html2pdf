c10fdd87d82f4e11fa2d10c2b4ba3563cbb3374a
Alex <alex@parse.cn>
2022-06-14 16:45:13 +0800

    wip

---
+++ app/Http/Controllers/PdfController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Spatie\Browsershot\Browsershot;
+
+class PdfController extends Controller
+{
+    public function __invoke()
+    {
+        $url    = request('url');
+        $pdf    = Browsershot::url($url);
+        $params = request()->except('url');
+
+        foreach ($params as $key => $value) {
+            $pdf->setOption($key, $value);
+        }
+
+        return response()->make($pdf->pdf(), 200, [
+            'Content-Type' => 'application/pdf',
+        ]);
+    }
+}
